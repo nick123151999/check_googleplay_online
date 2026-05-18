8ff5f923aca68dff1a9679f2381821eed59ff33b
Fortune tiger <nick123151999@gmail.com>
2026-05-18 22:18:33 +0700

    Update comp2.py

--- comp2.py
+++ comp2.py
@@ -11,7 +11,7 @@ APP_LIST = [
      "https://play.google.com/store/apps/details?id=com.todomaskj.toshhks2026",
     "https://play.google.com/store/apps/details?id=com.gamesters.gridora",
     "https://play.google.com/store/apps/details?id=com.tigerplinko.plinkogame",
-    "https://play.google.com/store/apps/details?id=com.idolive.fishingwars",
+    # "https://play.google.com/store/apps/details?id=com.idolive.fishingwars",
 ]
 
 def send_tg(msg):
