85610d2a82264875ffec2cf32bbd4e20a5f5df46
Fortune tiger <nick123151999@gmail.com>
2026-05-19 16:26:18 +0700

    Update check_apps.py

--- check_apps.py
+++ check_apps.py
@@ -13,8 +13,8 @@ APP_LIST = [
     "https://play.google.com/store/apps/details?id=com.todomaskj.toshhks2026",
     "https://play.google.com/store/apps/details?id=com.gamesters.gridora",
     "https://play.google.com/store/apps/details?id=com.tigerplinko.plinkogame",
-    "https://play.google.com/store/apps/details?id=com.idolive.fishingwars",
-    "https://play.google.com/store/apps/details?id=com.majiang.luckymajiang",
+    # "https://play.google.com/store/apps/details?id=com.idolive.fishingwars",
+    # "https://play.google.com/store/apps/details?id=com.majiang.luckymajiang",
 ]
 
 # 发送TG消息
