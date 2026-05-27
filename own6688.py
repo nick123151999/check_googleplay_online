0af3c84e2b42384cac6f2506961318994dcb472d
Fortune tiger <nick123151999@gmail.com>
2026-05-27 21:20:12 +0700

    Update own6688.py

--- own6688.py
+++ own6688.py
@@ -36,6 +36,7 @@ APP_LIST = [
     "https://play.google.com/store/apps/details?id=com.pandangame.majiangtooo003",
     "https://play.google.com/store/apps/details?id=com.pandamajiang.majiang004",
     "https://play.google.com/store/apps/details?id=com.pamdhh.majianggame005",
+    "https://play.google.com/store/apps/details?id=com.pamdhh.majianggame006",
     
     "https://play.google.com/store/apps/details?id=pak.al.nasir.bugzy.tap.hunting",
     "https://play.google.com/store/apps/details?id=com.nafay.drift",
