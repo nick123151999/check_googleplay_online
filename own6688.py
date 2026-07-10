77337c90fdb51485e13eb33f7c532cfc6de594fa
Fortune tiger <nick123151999@gmail.com>
2026-07-10 15:24:13 +0800

    Update own6688.py

--- own6688.py
+++ own6688.py
@@ -17,7 +17,7 @@ CHAT_IDS.extend(get_chat_ids("TG_CHAT_ID_OWN"))
 
 # 只保留链接，移除gp渠道编号
 APP_LIST = [
-    "https://play.google.com/store/apps/details?id=com.pandamajiang.panda001",
+    # "https://play.google.com/store/apps/details?id=com.pandamajiang.panda001",
     "https://play.google.com/store/apps/details?id=com.game78.fishgoo",
     "https://play.google.com/store/apps/details?id=com.khalidy.fortunecatch",
     "https://play.google.com/store/apps/details?id=com.idolive.fishingwars",
