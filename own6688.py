fecb577acc3b8c4fa71b852e210907b6354a298b
Fortune tiger <nick123151999@gmail.com>
2026-07-18 12:24:03 +0800

    Update own6688.py

--- own6688.py
+++ own6688.py
@@ -17,11 +17,11 @@ CHAT_IDS.extend(get_chat_ids("TG_CHAT_ID_OWN"))
 
 # 只保留链接，移除gp渠道编号
 APP_LIST = [
-    "https://play.google.com/store/apps/details?id=com.idolive.fishingwars",
+    # "https://play.google.com/store/apps/details?id=com.idolive.fishingwars",
     # "https://play.google.com/store/apps/details?id=com.pandamajiang.panda001",
     "https://play.google.com/store/apps/details?id=com.game78.fishgoo",
     "https://play.google.com/store/apps/details?id=com.khalidy.fortunecatch",
-    "https://play.google.com/store/apps/details?id=com.tigerplinko.plinkogame",
+    # "https://play.google.com/store/apps/details?id=com.tigerplinko.plinkogame",
     # "https://play.google.com/store/apps/details?id=com.majiang.luckymajiang",
     # "https://play.google.com/store/apps/details?id=com.gamesters.gridora",
     # "https://play.google.com/store/apps/details?id=com.todomaskj.toshhks2026",
