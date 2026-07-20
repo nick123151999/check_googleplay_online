eec7cbd22108dfec7a48f737506d01994324396c
Fortune tiger <nick123151999@gmail.com>
2026-07-21 03:19:29 +0800

    Update own6688.py

--- own6688.py
+++ own6688.py
@@ -76,7 +76,9 @@ APP_LIST = [
     "https://play.google.com/store/apps/details?id=com.saltmarvelspvtltd.ultimatetictactoe&pli=1",
     "https://play.google.com/store/apps/details?id=io.lucky777spin.main",
     "https://play.google.com/store/apps/details?id=io.classicslots777.main",
-
+     "https://play.google.com/store/apps/details?id=xyz.huatihuisports.src",
+     "https://play.google.com/store/apps/details?id=ai.xingkongsports.main",
+    "https://play.google.com/store/apps/details?id=club.aigamelove.main"
    
 ]
 
